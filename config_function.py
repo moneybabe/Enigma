62ea2a56618c7e04c2fd94ccbbb97bd42f169ace
Neo Lee <neo.lky852@gmail.com>
2022-11-15 01:54:07 -0800

    handled incorrect input for plugboard config

--- config_function.py
+++ config_function.py
@@ -1,39 +1,52 @@
 def config():
     # print instruction for plugboard configurations
-    print('\nYou can set up to 10 configurations for the plugboard ')
-    print('Press enter when you finished configuring \n')
+    print('\nYou can set up to 10 configurations for the plugboard, press enter to finish configuring')
 
     # request input of plugboard setting, and store the settings as a list
     plugboard_setting = []
-    flag = False
     for i in range(10):
-        setting = input('Enter your plugboard configurations in the following format: \'a,c\' \n').lower()
+        lst = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth']
+        setting = input('Enter the ' + lst[i] + ' two letters you want to connect in plugboard (examples: pt or yo): ').lower()
+        
+        # check if format is correct
+        break_setting = False
+        format_is_wrong = True
+        while format_is_wrong:
+            format_is_wrong = False
 
-        # finish config if user enter nothing
-        if setting == '':
-            print('You have finished configuring, this is your final setting')
-            break
-
-        # finish config if first or last character is not from a-z
-        if (not 97 <= ord(setting[0]) <=122) or (not 97 <= ord(setting[-1]) <= 122):
-            print('You trolling, this is your final setting')
-            break
-
-        # finish config if setting repeated
-        for x in plugboard_setting:
-            if setting[0] in x or setting[-1] in x:
-                print('You trolling, this is your final setting')
-                flag = True
+            # finish config if user enter nothing
+            if setting == '':
+                print('You have finished configuring, this is your final setting: ', end = '')
+                print(plugboard_setting)
+                break_setting = True
                 break
-        
-        if flag:
-            break
-        
+
+            # reenter if input is not two characters
+            if len(setting) != 2:
+                setting = input('You entered more than two characters, please reenter: ')
+                format_is_wrong = True
+
+            # reenter if not within letters range
+            elif (not 97 <= ord(setting[0]) <=122) or (not 97 <= ord(setting[-1]) <= 122):
+                setting = input('You entered something other than letters, please reenter: ')
+                format_is_wrong = True
+
+            # reenter if setting has repeasted
+            else:
+                for x in plugboard_setting:
+                    if setting[0] in x or setting[-1] in x:
+                        setting = input('Your setting has repeated, please reenter: ')
+                        format_is_wrong = True
+
         # append input setting into plugboard_setting list
         plugboard_setting.append(setting)
 
-    # print final plugboard_setting
-    print(plugboard_setting)
+        if break_setting:
+            break
+
+        # print plugboard_setting
+        print('Your current plugboard setting: ', end = '')
+        print(plugboard_setting)
 
     # request order of rotors
     print('Choose three rotors from a set of five and place it in order')
@@ -42,7 +55,7 @@ def config():
     third_rotor = int(input('Which one is the third rotor? '))
 
     # request initial position of rotors
-    print('\nPlease enter the rotors\' configurations')
+    print('Please enter the rotors\' configurations')
     rotor1_position = int(input('Enter the initial position of rotor 1: '))
     rotor2_position = int(input('Enter the initial position of rotor 2: '))
     rotor3_position = int(input('Enter the initial position of rotor 3: '))
