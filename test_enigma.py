3aafa579dc835c77b7f8b42277509a1a7a317207
Neo <yl003@ucsd.edu>
2022-12-02 11:23:43 +0000

    added comment for test

--- test_enigma.py
+++ test_enigma.py
@@ -10,6 +10,7 @@ from testing_modules.mock_input_output_module import set_keyboard_input, get_dis
 
 '''Test all functions'''
 
+# test plugboard_config function returns correct config even with invalid input
 def test_plugboard_config():
     set_keyboard_input(['123', 'ab', ''])
     plugboard_setting = pb.plugboard_config()
@@ -26,32 +27,38 @@ def test_plugboard_config():
     ]
 
 
+# test rotor_order_config function returns correct config even with invalid input
 def test_rotor_order_config():
     set_keyboard_input(['1', '6', '3', '1', ' ', '!@#', 'random', '5'])
     assert roc.rotor_order_config() == [1, 3, 5]
 
 
+# test rotor_position_config function returns correect config even with invalid input
 def test_rotor_position_config():
     set_keyboard_input(['-7', '12', '26', 'random str', '#!@$', ' ', '4', '17'])
     assert rpc.rotor_position_config() == [12, 4, 17]
 
 
+# test overall_config_function returns correct config
 def test_overall_config_function():
     set_keyboard_input(['qw', 'er', 'ty', '', '4', '2', '1', '', '23', '5', '13'])
     assert ocf.config() == [['qw', 'er', 'ty'], [4, 2, 1], [23, 5, 13]]
 
 
+# test request_message function returns correct config even with invalid input
 def test_request_message():
-    set_keyboard_input(['!@#', 'dwd23fefw', 'random     string'])
+    set_keyboard_input(['!@#', 'dwd23fefw', 'raNdoM     sTrINg'])
     output = ef.request_message()
     assert output == 'random     string'
 
 
+# test encryption_function can encrypt 'hello world' correctly 
 def test_encryption_function():
     config_lst = [['ab', 'cd', 'ef'], [1, 4, 5], [15, 24, 6]]
     assert ef.cipher_message(config_lst, 'hello world') == 'EYHYI FCWJQ'
 
 
+# test main_function can encrypt a random message and decrypting the message will return the correct plain text
 def test_main_function():
     random_string = ''.join(random.choices(string.ascii_lowercase, k=10))
     set_keyboard_input(['ab', 'cd', 'ef', '', '4', '2', '5', '21', '9', '13', random_string, 'n'])
